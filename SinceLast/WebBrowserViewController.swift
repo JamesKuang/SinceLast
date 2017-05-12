409143418ea92e89b5e3b8401d9af61f719f0d70
James Kuang <kuang812@gmail.com>
2017-05-12 09:45:48 -0400

    Simple WebBrowserViewController for auth so we can internal control cookie & cache policies

---
+++ SinceLast/WebBrowserViewController.swift
@@ -0,0 +1,51 @@
+//
+//  WebBrowserViewController.swift
+//  SinceLast
+//
+//  Created by James Kuang on 5/12/17.
+//  Copyright © 2017 Incyc. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+final class WebBrowserViewController: UIViewController {
+    let webView: WKWebView = {
+        let webView = WKWebView()
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        return webView
+    }()
+
+    let request: URLRequest
+
+    convenience init(url: URL) {
+        let request = URLRequest(url: url)
+        self.init(request: request)
+    }
+
+    init(request: URLRequest) {
+        self.request = request
+        super.init(nibName: nil, bundle: nil)
+
+        view.addSubview(webView)
+
+        NSLayoutConstraint.activate([
+            webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            webView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            webView.topAnchor.constraint(equalTo: view.topAnchor),
+            webView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            ])
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        if isMovingToParentViewController {
+            webView.load(request)
+        }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
