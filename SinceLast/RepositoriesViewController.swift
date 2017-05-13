7b696de0d1385a9f75fdb2f094be7fb13c1a5b02
James Kuang <kuang812@gmail.com>
2017-05-13 17:26:12 -0400

    Clean up AppDelegate

---
+++ SinceLast/RepositoriesViewController.swift
@@ -0,0 +1,24 @@
+//
+//  RepositoriesViewController.swift
+//  SinceLast
+//
+//  Created by James Kuang on 5/13/17.
+//  Copyright © 2017 Incyc. All rights reserved.
+//
+
+import UIKit
+
+final class RepositoriesViewController: UIViewController {
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        title = NSLocalizedString("Repositories", comment: "Repositories screen navigation bar title")
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
