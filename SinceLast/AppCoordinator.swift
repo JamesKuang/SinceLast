621a4fe4a09ff8a2b8e68f5b594d0cfb54886459
James Kuang <kuang812@gmail.com>
2017-05-13 22:21:18 -0400

    App coordinator

---
+++ SinceLast/AppCoordinator.swift
@@ -0,0 +1,42 @@
+//
+//  AppCoordinator.swift
+//  SinceLast
+//
+//  Created by James Kuang on 5/13/17.
+//  Copyright © 2017 Incyc. All rights reserved.
+//
+
+import UIKit
+
+final class AppCoordinator {
+    private(set) var rootViewController: UIViewController?
+
+    var isAuthorized: Bool {
+        return false // TODO:
+    }
+
+    init() {
+
+    }
+
+    func startLaunchViewController() -> UIViewController {
+        let controller: UIViewController
+        if isAuthorized {
+            controller = RepositoriesViewController()
+        } else {
+            let services = [BitbucketService()]
+            controller = GitServicesAuthorizationViewController(services: services)
+        }
+
+        let rootViewController = UINavigationController(rootViewController: controller)
+        self.rootViewController = rootViewController
+        return rootViewController
+    }
+
+    func handleOAuthURL(_ url: URL) -> Bool {
+        let validator = OAuthURLValidator(url: url)
+        guard validator.isValid else { return false }
+
+        return true
+    }
+}
--- SinceLast/AppDelegate.swift
+++ SinceLast/AppDelegate.swift
@@ -10,28 +10,16 @@ import UIKit
 
 @UIApplicationMain
 final class AppDelegate: UIResponder, UIApplicationDelegate {
-
-    var window: UIWindow?
+    lazy var window: UIWindow? = UIWindow(frame: UIScreen.main.bounds)
+    lazy var coordinator: AppCoordinator = AppCoordinator()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        let isAuthorized = false
-
-        let controller: UIViewController
-        if isAuthorized {
-            controller = RepositoriesViewController()
-        } else {
-            let services = [BitbucketService()]
-            controller = GitServicesAuthorizationViewController(services: services)
-        }
-
-        window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = UINavigationController(rootViewController: controller)
+        window?.rootViewController = coordinator.startLaunchViewController()
         window?.makeKeyAndVisible()
-
         return true
     }
 
     func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
-        return true
+        return coordinator.handleOAuthURL(url)
     }
 }
