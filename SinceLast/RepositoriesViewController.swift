abe9333e9ce5f2672fb879a940a046c556d37df4
James Kuang <kuang812@gmail.com>
2017-05-22 23:35:55 -0400

    Began work on repository list UI

--- SinceLast/RepositoriesViewController.swift
+++ SinceLast/RepositoriesViewController.swift
@@ -12,10 +12,28 @@ import PromiseKit
 final class RepositoriesViewController: UIViewController, GitClientRequiring {
     let gitClient: GitClient
 
+    private let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
+
+    fileprivate var repositores: [Repository] = []
+
     init(client: GitClient) {
         self.gitClient = client
         super.init(nibName: nil, bundle: nil)
         title = NSLocalizedString("Repositories", comment: "Repositories screen navigation bar title")
+
+        view.addSubview(collectionView)
+
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            ])
     }
 
     required init?(coder aDecoder: NSCoder) {
@@ -24,6 +42,9 @@ final class RepositoriesViewController: UIViewController, GitClientRequiring {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        collectionView.register(cell: RepositoryCell.self)
+        collectionView.dataSource = self
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -38,8 +59,8 @@ final class RepositoriesViewController: UIViewController, GitClientRequiring {
         let _ = retrieveUser()
             .then { user in
                 self.retrieveRepositories(for: user)
-            }.then { repository in
-                print(repository)
+            }.then { repositories in
+                self.reload(with: repositories)
             }.catch { error in
                 print(error)
         }
@@ -56,4 +77,60 @@ final class RepositoriesViewController: UIViewController, GitClientRequiring {
             return result.repositories
         })
     }
+
+    private func reload(with repositories: [Repository]) {
+        print(repositories)
+        self.repositores = repositories
+        self.collectionView.reloadData()
+    }
+}
+
+extension RepositoriesViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return repositores.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueCell(of: RepositoryCell.self, for: indexPath)
+        let repository = repositores[indexPath.item]
+        cell.configure(with: repository)
+        return cell
+    }
+}
+
+final class RepositoryCell: UICollectionViewCell {
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        contentView.addSubview(titleLabel)
+
+        let guide = contentView.readableContentGuide
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: guide.topAnchor),
+            titleLabel.bottomAnchor.constraint(equalTo: guide.bottomAnchor),
+            titleLabel.leadingAnchor.constraint(equalTo: guide.leadingAnchor),
+            titleLabel.trailingAnchor.constraint(equalTo: guide.trailingAnchor),
+            ])
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension RepositoryCell: ConfigurableCell {
+    func configure(with object: Repository) {
+        titleLabel.text = object.name
+    }
+}
+
+protocol ConfigurableCell {
+    associatedtype T
+    func configure(with object: T)
 }
