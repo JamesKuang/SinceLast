9fdda081d0bf020e91c8a4ad99176d85c6751f96
James Kuang <kuang812@gmail.com>
2017-07-04 00:44:26 -0400

    Pagination Tests

--- SinceLastTests/PaginationTests.swift
+++ SinceLastTests/PaginationTests.swift
@@ -6,4 +6,28 @@
 //  Copyright © 2017 Incyc. All rights reserved.
 //
 
-import Foundation
+import XCTest
+@testable import SinceLast
+
+class PaginationTests: XCTestCase {
+    func testHasNextPage() {
+        XCTAssertTrue(Pagination.initial.hasNextPage)
+        XCTAssertTrue(Pagination.integer(1).hasNextPage)
+        XCTAssertTrue(Pagination.cursor("abc").hasNextPage)
+        XCTAssertFalse(Pagination.none.hasNextPage)
+    }
+
+    func testIntegerPage() {
+        XCTAssertEqual(Pagination.initial.integerPage, 1)
+        XCTAssertEqual(Pagination.integer(1).integerPage, 1)
+        XCTAssertNil(Pagination.cursor("abc").integerPage)
+        XCTAssertNil(Pagination.none.integerPage)
+    }
+
+    func testCursorPage() {
+        XCTAssertEqual(Pagination.cursor("abc").cursorPage, "abc")
+        XCTAssertNil(Pagination.initial.cursorPage)
+        XCTAssertNil(Pagination.integer(1).cursorPage)
+        XCTAssertNil(Pagination.none.cursorPage)
+    }
+}
