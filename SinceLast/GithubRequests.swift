00209c282f82d9677e4e1426e56bbe316240bf63
James Kuang <kuang812@gmail.com>
2017-07-03 23:42:24 -0400

    Support pageInfo and parsing into result

--- SinceLast/GithubRequests.swift
+++ SinceLast/GithubRequests.swift
@@ -14,6 +14,10 @@ protocol GithubGraphTraversing {
     static var connections: [String] { get }
 }
 
+protocol GithubGraphPaginating {
+    static var pageInfo: [String] { get }
+}
+
 extension GithubTypedRequest {
     var method: RequestMethod {
         return .POST
@@ -42,18 +46,23 @@ struct GithubUserRequest: GithubTypedRequest {
     }
 }
 
-struct GithubRepositoriesRequest: GithubTypedRequest, GithubGraphTraversing {
+struct GithubRepositoriesRequest: GithubTypedRequest, GithubGraphTraversing, GithubGraphPaginating {
     typealias ResultType = GithubArrayResult<GithubRepository, GithubRepositoriesRequest>
 
     let page: Int
+    let pageSize = 20
 
     var bodyParameters: [String : Any] {
         return [
-            "query": "query { viewer { repositories(first: 50, orderBy: {field: PUSHED_AT, direction: DESC}) { edges { node { id name description owner { id login } } } } } }",
+            "query": "query { viewer { repositories(first: \(pageSize), orderBy: {field: PUSHED_AT, direction: DESC}) { pageInfo { endCursor hasNextPage } edges { node { id name description owner { id login } } } } } }",
         ]
     }
 
     static var connections: [String] {
         return ["data", "viewer", "repositories", "edges"]
+    }
+
+    static var pageInfo: [String] {
+        return ["data", "viewer", "repositories", "pageInfo"]
     }
 }
--- SinceLast/GithubResults.swift
+++ SinceLast/GithubResults.swift
@@ -8,24 +8,39 @@
 
 import Foundation
 
-struct GithubArrayResult<T: JSONInitializable, U: GithubGraphTraversing>: JSONInitializable {
+struct GithubArrayResult<T: JSONInitializable, U>: JSONInitializable where U: GithubGraphTraversing, U: GithubGraphPaginating {
     let objects: [T]
+    let hasNextPage: Bool
+    let endCursor: String
 
     init(json: JSON) throws {
-        let connections = U.connections
-        var dictionary = json
-
-        for connection in connections {
-            let value = dictionary[connection]
-            if let next = value as? JSON {
-                dictionary = next
-            } else if let values = value as? [JSON] {
-                self.objects = try values.flatMap { try T(json: $0) }
-                return
-            } else {
-                break
+        func findJSON<X>(traversals: [String], in json: JSON) throws -> X {
+            var dictionary = json
+            for traversal in traversals {
+                let value = dictionary[traversal]
+                if let next = value as? JSON {
+                    dictionary = next
+                } else if let values = value as? X {
+                    return values
+                } else {
+                    break
+                }
             }
+
+            if let values = dictionary as? X {
+                return values
+            }
+            throw JSONParsingError()
         }
-        throw JSONParsingError()
+
+        let pageInfo: JSON = try findJSON(traversals: U.pageInfo, in: json)
+        guard let hasNextPage = pageInfo["hasNextPage"] as? Bool,
+            let endCursor = pageInfo["endCursor"] as? String
+            else { throw JSONParsingError() }
+        self.hasNextPage = hasNextPage
+        self.endCursor = endCursor
+
+        let edges: [JSON] = try findJSON(traversals: U.connections, in: json)
+        self.objects = try edges.flatMap { try T(json: $0) }
     }
 }
